874ceb28b9b392d68c38ba24bc15e34d1bb0c65d
Christian-K9 <164112138+Christian-K9@users.noreply.github.com>
2025-03-21 17:08:04 -0400

    installer

--- server_setup.py
+++ server_setup.py
@@ -17,20 +17,14 @@ def install_package(package, installer):
         subprocess.check_call(["sudo", installer, "install", "-y", package])
     except subprocess.CalledProcessError as e:
         print("Error Installing: " + package)
-def check_service_status(service_name, attempt):
+
+def check_service_status(service_name):
     result = subprocess.run(["systemctl", "is-active", service_name],
                             capture_output = True, text = True)
     if result.stdout.strip() == 'active':
         print(service_name + " Is Running")
     else:
-        print(service_name + " Is Not Running")
-        if attempt < 3:
-            print("Attempting To Try Again")
-            install_package(service_name, attempt)
-        else:
-            print()
-            print(service_name + " Could Not Be Installed")
-            print("Check Configuration Files")
+        print(service_name + " Could Not Be Installed ")
 
     #Declare Machine
 machines = {"debian": "apt", "ubuntu": "apt",
@@ -65,8 +59,11 @@ while server_type == None:
 for i in server_type:
         print("Installing " + i)
         install_package(i, installer)
+        check_service_status(i)
 
 #Configure Package
+
 #Start/Enable Services
+
 #Test Capability
 #Summarization
