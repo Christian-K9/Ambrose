6ff3d5e29c3f4360e8537163a608c7b57f8b848e
Christian-K9 <164112138+Christian-K9@users.noreply.github.com>
2025-09-09 19:35:32 -0400

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -1,12 +1,32 @@
-//
-// Do any local configuration here
-//
+#!/bin/bash
+read -p "Server Or Client? [s/c]: " machine_type
+if [ "$machine_type" = "s" ]; then
+        sudo systemctl enable named
+        sudo systemctl start named
+        read -p "Enter Network ID: " network_id
+        read -p "Enter Host Identifier: " machine_ip
+        fq_ip="${network_id}.${machine_ip}"
+        server=$((num + 10)
+        full_rev_zone="$(echo "$network_id" | awk -F. '{print $3"."$2"."$1}').in-addr.arpa"
+        #named.conf.local file
+        sudo tee -a named.conf.local >/dev/null <<ENDZONE
+        zone "$full_rev_zone" {
+                type master;
+                file "/etc/bind/db.$network_id";
+        };
+ENDZONE
 
-// Consider adding the 1918 zones here, if they are not used in your
-// organization
-//include "/etc/bind/zones.rfc1918";
-
-zone "ccdc.local" {
-        type master;
-        file "/etc/bind/db.ccdc.local";
-}
+        cp named.conf /etc/bind/named.conf
+        #db.ccdc.local file
+        sudo tee -a db.ccdc.local >/dev/null <<ENDZ
+        ns1     IN      A       $fq_ip
+        srv     IN      A       $network_id.server
+ENDZ
+        cp db.ccdc.local /etc/bind/db.ccdc.local
+        sudo tee -a /db.$network_id >/dev/null <<EOF
+        $machine_ip     IN      PTR     ns1.ccdc.local.
+        $machine_ip     IN      PTR     srv.ccdc.local.
+EOF
+fi
+~
+(END)
