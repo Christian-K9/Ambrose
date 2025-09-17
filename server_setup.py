7b38fb5bf6313d542d5cd29a875742b6c99e274f
Christian-K9 <164112138+Christian-K9@users.noreply.github.com>
2025-09-16 21:52:14 -0400

    Update server_setup.py

--- server_setup.py
+++ server_setup.py
@@ -43,7 +43,7 @@ while installer == None:
 
     #Declare Server
 servers = {"ftp": ["vsftpd"], "tftp" : ["tftpd-server", "tftpd-hpa"], 
-           "sftp": ["openssh-server"], "telnet": ["telnet-server xinetd"],
+           "sftp": ["openssh-server"], "telnet": ["telnetd"],
             "dns": ["bind9", "bind9-utils", "bind9-dnsutils"], "ntp": ["chronyd"]}
 server_type = None
 while server_type == None:
